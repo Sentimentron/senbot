1c2224ab1c40b9fee2093c3e015f082d09d480dc
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-03-18 12:02:32 +0000

    Hopefully even more memory efficient.

--- tasks.py
+++ tasks.py
@@ -4,13 +4,15 @@ from celery import Task, registry
 from core import get_celery, get_database_engine_string
 from lookup.models import WhitespaceExpansionTrieNode, UnambiguousTrieNode
 from models import Keyword, KeywordAdjacency, KeywordIncidence 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.exc import *
 from sqlalchemy.orm import * 
 from sqlalchemy.orm.exc import *
 from sqlalchemy.orm.session import Session 
 from sqlalchemy.pool import SingletonThreadPool
 import string 
+import MySQLdb.cursors
+
 
 import logging
 
@@ -32,8 +34,10 @@ class ProdWhiteSpaceKWExpand(object):
         # Database connection
         engine = get_database_engine_string()
         logging.info("Using connection string '%s'" % (engine,))
-        engine = create_engine(engine, encoding='utf-8', isolation_level = 'READ UNCOMMITTED', poolclass=SingletonThreadPool, echo = False)
-        session = Session(bind=engine)
+        engine = create_engine(engine, encoding='utf-8', isolation_level = 'READ UNCOMMITTED', poolclass=SingletonThreadPool, echo = False, connect_args={'cursorclass': MySQLdb.cursors.SSCursor})
+        meta = MetaData(engine, reflect=True)
+        conn = engine.connect()
+        session = Session(bind=conn)
 
         # Query for keywords
         for k in session.query(Keyword):
