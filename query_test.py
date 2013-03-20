7cfe57a469ddcc3035cd67228d940be6afd88ca4
Ubuntu <ubuntu@domU-12-31-39-0B-18-21.compute-1.internal>
2013-03-20 10:59:15 +0000

    Changed to spaces

--- query_test.py
+++ query_test.py
@@ -10,54 +10,56 @@ import types
 celery = get_celery()
 
 queries = ["Barack", "McCain",
-	"+Barack AND McCain foxnews.com",
-	"+Barack AND -\"McCain Oven Chips\" foxnews.com"
+    "+Barack AND McCain foxnews.com",
+    "+Barack AND -\"McCain Oven Chips\" foxnews.com"
 ]
 
 def expand_keyword(keyword):
-	if type(keyword) != QueryKeyword:
-		return keyword
-	kw = keyword.keyword
-	return celery.send_task("cache.ProdWhiteSpaceKWExpand", [kw])
+    if type(keyword) != QueryKeyword:
+        return keyword
+    kw = keyword.keyword
+    return celery.send_task("cache.ProdWhiteSpaceKWExpand", [kw])
 
 def resolve_keyword(keyword):
-	kw = None
-	if type(keyword) == type(set([])):
-		return [resolve_keyword(i) for i in keyword]
-	if type(keyword) == types.StringType:
-		if keyword == "AND" or keyword == "OR":
-			return keyword 
-		kw = keyword
-	elif type(keyword) == QueryKeyword:
-		kw = keyword.keyword 
-	else:
-		return keyword 
+    kw = None
+    if type(keyword) == type(set([])):
+        return [resolve_keyword(i) for i in keyword]
+    if type(keyword) == types.StringType:
+        if keyword == "AND" or keyword == "OR":
+            return keyword 
+        kw = keyword
+    elif type(keyword) == QueryKeyword:
+        kw = keyword.keyword
+    elif type(keyword) == AsyncResult:
+        kw = keyword
+    else:
+        return keyword 
 
-	assert kw != None 
-	return celery.send_task("tasks.ProdKWIdentityResolve", [keyword])
+    assert kw != None 
+    return celery.send_task("tasks.ProdKWIdentityResolve", [keyword])
 
 def resolve(result):
-	print "RESOLVE",type(result), isinstance(result, AsyncResult)
-	if not isinstance(result, AsyncResult):
-		return result 
-	try:
-		return result.get()
-	except TimeoutError:
-		return None 
+    print "RESOLVE",type(result), isinstance(result, AsyncResult)
+    if not isinstance(result, AsyncResult):
+        return result 
+    try:
+        return result.get()
+    except TimeoutError:
+        return None 
 
 for c, q in enumerate(queries):
 
-	if c != 2:
-		continue
-	
-	# Parse query 
-	parsed = query.parseString(q).asList()
-	print c
-	print "PARSED", parsed
-	
-	# Got a problem: ignores literal keyword modifiers
-	inter = parsed
-	inter = recursive_map(inter, lambda x: expand_keyword(x))
-	inter = recursive_map(inter, lambda x: resolve_keyword(x))
-	inter = recursive_map(inter, lambda x: resolve(x))
-	print inter 
+    if c != 2:
+        continue
+    
+    # Parse query 
+    parsed = query.parseString(q).asList()
+    print c
+    print "PARSED", parsed
+    
+    # Got a problem: ignores literal keyword modifiers
+    inter = parsed
+    inter = recursive_map(inter, lambda x: expand_keyword(x))
+    inter = recursive_map(inter, lambda x: resolve_keyword(x))
+    inter = recursive_map(inter, lambda x: resolve(x))
+    print inter 
