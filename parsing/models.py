095b6c50254911ac137a7be08e0d42973d655119
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-03-22 12:48:21 +0000

    Spaces.

--- parsing/models.py
+++ parsing/models.py
@@ -3,160 +3,160 @@
 # Object specification 
 class ParseContainer(object):
 
-	def __init__(self, s, loc, toks):
-		self.s, self.loc, self.toks = s, loc, toks
+    def __init__(self, s, loc, toks):
+        self.s, self.loc, self.toks = s, loc, toks
 
-	def __repr__(self):
-		return "%s(%s)" % (type(self), {'s': self.s, 'loc': self.loc, 'toks':self.toks})
+    def __repr__(self):
+        return "%s(%s)" % (type(self), {'s': self.s, 'loc': self.loc, 'toks':self.toks})
 
 
 class QueryDomain(ParseContainer):
-	
-	def __init__(self, s, loc, toks):
-		self.domain = ''.join(toks) 
+    
+    def __init__(self, s, loc, toks):
+        self.domain = ''.join(toks) 
 
-	def __repr__(self):
-		return "QueryDomain(%s)" % (self.domain,)
+    def __repr__(self):
+        return "QueryDomain(%s)" % (self.domain,)
 
 class QueryKeyword(ParseContainer):
 
-	def __init__(self, s, loc, toks):
-		for keyword in toks:
-			self.keyword = keyword 
+    def __init__(self, s, loc, toks):
+        for keyword in toks:
+            self.keyword = keyword 
 
-	@classmethod
-	def from_str(self, s):
-		return QueryKeyword(None, None, [s])
+    @classmethod
+    def from_str(self, s):
+        return QueryKeyword(None, None, [s])
 
-	def __repr__(self):
-		return "QueryKeyword(%s)" % (self.keyword,)
+    def __repr__(self):
+        return "QueryKeyword(%s)" % (self.keyword,)
 
 class QueryKeywordModifier(ParseContainer):
 
-	def __init__(self, item):
-		self.item = item
+    def __init__(self, item):
+        self.item = item
 
-	def __repr__(self):
-		return "%s(%s)" % (type(self), self.item)
+    def __repr__(self):
+        return "%s(%s)" % (type(self), self.item)
 
 class QueryKeywordExclusionModifier(QueryKeywordModifier):
-	pass 
+    pass 
 
 class QueryKeywordLiteralModifier(QueryKeywordModifier):
-	pass 
+    pass 
 
 class QueryContainer(object):
 
-	def __init__(self, _list=[]):
-		self._list = _list
+    def __init__(self, _list=[]):
+        self._list = _list
 
-	def __len__(self):
-		return len(self._list)
+    def __len__(self):
+        return len(self._list)
 
-	def __iter__(self):
-		for item in self._list:
-			yield item 
+    def __iter__(self):
+        for item in self._list:
+            yield item 
 
-	def __reversed__(self):
-		return reversed(self._list)
+    def __reversed__(self):
+        return reversed(self._list)
 
-	def __repr__(self):
-		return "%s(%s)" % (type(self), self._list)
+    def __repr__(self):
+        return "%s(%s)" % (type(self), self._list)
 
-	def append(self, item):
-		self._list.append(item)
+    def append(self, item):
+        self._list.append(item)
 
-	def __getitem__(self, k):
-		return self._list[k]
+    def __getitem__(self, k):
+        return self._list[k]
 
-	def __setitem__(self, k, v):
-		self._list[k] = v
+    def __setitem__(self, k, v):
+        self._list[k] = v
 
 class QueryJoinOperator(QueryContainer):
-	def __repr__(self):
-		return "%s(%s)" % ("QueryJoinOperator", self._list)
+    def __repr__(self):
+        return "%s(%s)" % ("QueryJoinOperator", self._list)
 
-	def aggregate(self):
-		pass 
+    def aggregate(self):
+        pass 
 
 class QueryIntersection(QueryJoinOperator):
-	def __repr__(self):
-		return "%s(%s)" % ("QueryIntersection", self._list)
+    def __repr__(self):
+        return "%s(%s)" % ("QueryIntersection", self._list)
 
-	def aggregate(self):
-		ret = None
-		for i in self:
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.intersection(i)
-		return list(ret)
+    def aggregate(self):
+        ret = None
+        for i in self:
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.intersection(i)
+        return list(ret)
 
 class QueryDifference(QueryJoinOperator):
-	def __repr__(self):
-		return "%s(%s)" % ("QueryIntersection", self._list)
+    def __repr__(self):
+        return "%s(%s)" % ("QueryIntersection", self._list)
 
-	def aggregate(self):
-		ret = None 
-		for i in reversed(self):
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.difference(i)
-		return list(ret)
+    def aggregate(self):
+        ret = None 
+        for i in reversed(self):
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.difference(i)
+        return list(ret)
 
 class QueryUnion(QueryJoinOperator):
-	def __repr__(self):
-		return "%s(%s)" % ("QueryUnion", self._list)
+    def __repr__(self):
+        return "%s(%s)" % ("QueryUnion", self._list)
 
-	def aggregate(self):
-		ret = None 
-		for i in self:
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.union(i)
-		return list(ret)
+    def aggregate(self):
+        ret = None 
+        for i in self:
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.union(i)
+        return list(ret)
 
 class Query(QueryContainer):
 
-	def aggregate(self):
-		pass 
+    def aggregate(self):
+        pass 
 
 class OrQuery(Query):
 
-	def aggregate(self):
-		ret = None 
-		for i in self:
+    def aggregate(self):
+        ret = None 
+        for i in self:
             if isinstance(i, Query):
                 i = i.aggregate()
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.union(i)
-		return list(ret)
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.union(i)
+        return list(ret)
 
 class AndQuery(Query):
-	def aggregate(self):
-		ret = None
-		for i in self:
+    def aggregate(self):
+        ret = None
+        for i in self:
             if isinstance(i, Query):
                 i = i.aggregate()
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.intersection(i)
-		return list(ret)
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.intersection(i)
+        return list(ret)
 
 class NotQuery(Query):
 
-	def aggregate(self):
-		ret = None 
-		for i in reversed(self):
+    def aggregate(self):
+        ret = None 
+        for i in reversed(self):
             if isinstance(i, Query):
                 i = i.aggregate()
-			if ret is None:
-				ret = set(i)
-				continue 
-			ret = ret.difference(i)
-		return list(ret)
+            if ret is None:
+                ret = set(i)
+                continue 
+            ret = ret.difference(i)
+        return list(ret)
